456b00f7fa6d5c083f520ec67a2e1cba20766dc4
El'endia Starman <lee314b@gmail.com>
2015-10-01 00:58:38 -0400

    Added home_view. Git test.

--- dotandcross/views.py
+++ dotandcross/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.core.urlresolvers import reverse
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+
+from django.template import Context, RequestContext
 
 # Create your views here.
-def home_view():
+def home_view(request, **kwargs):
     pass
+
+    return render(request, 'dotandcross/home.html', context_instance=context)
