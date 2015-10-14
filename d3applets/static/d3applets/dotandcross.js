368b0b9cfad75bd82bcc230ed1b9d54256870f3e
El'endia Starman <lee314b@gmail.com>
2015-10-14 17:17:51 -0400

    Added minkolang app.

--- d3applets/static/d3applets/dotandcross.js
+++ d3applets/static/d3applets/dotandcross.js
@@ -1,3 +1,5 @@
+var svg = d3.select('svg');
+
 var arc = d3.svg.arc()
 	.startAngle(function(d){
 		return Math.atan2(d[0].attr("cy")-d[2].attr("cy"),
@@ -9,8 +11,7 @@ var arc = d3.svg.arc()
 	})
 	.innerRadius(0)
 	.outerRadius(30);
-
-d3.select('svg').append("path")
+svg.append("path")
 	.data([[d3.select('#red'),d3.select('#blue'),d3.select('#origin')]])
 	.attr("d", arc)
 	.attr("id", "arc")
@@ -18,25 +19,47 @@ d3.select('svg').append("path")
 	.style("fill", "yellow")
 	.style("opacity", 0.6);
 
+function dotproduct() {
+	var oc = d3.select("#origin");
+	return Math.round(
+	 (d3.select("#red").attr("cx")-oc.attr("cx"))*(d3.select("#blue").attr("cx")-oc.attr("cx"))
+	+(d3.select("#red").attr("cy")-oc.attr("cy"))*(d3.select("#blue").attr("cy")-oc.attr("cy"))
+	);
+};
+function crossproduct() {
+	var oc = d3.select("#origin");
+	return Math.round(
+	 (d3.select("#red").attr("cx")-oc.attr("cx"))*(d3.select("#blue").attr("cy")-oc.attr("cy"))
+	-(d3.select("#red").attr("cy")-oc.attr("cy"))*(d3.select("#blue").attr("cx")-oc.attr("cx"))
+	);
+};
+
+var dptext = svg.append("text")
+	.attr("x", 50)
+	.attr("y", 15)
+	.attr("id", "dptext")
+	.attr("class", "ptext")
+	.text( dotproduct() );
+svg.append("text")
+	.attr("x", 50)
+	.attr("y", 30)
+	.attr("id", "cptext")
+	.attr("class", "ptext")
+	.text( crossproduct() );
+	
 var drag = d3.behavior.drag()
 	.on('drag', function(d){
 		var c = d3.select(this)
 		c.attr("cx", d3.event.x).attr("cy", d3.event.y);
 		if (c.attr("id") == "origin") {
 			d3.selectAll("line").attr("x1", d3.event.x).attr("y1", d3.event.y);
+			d3.select("#arc").attr("transform", "translate("+c.attr("cx")+","+c.attr("cy")+")");
 		} else {
 			d3.select("[name="+c.attr("id")+"]").attr("x2", d3.event.x).attr("y2", d3.event.y);
 		}
 		
-		var rc = d3.select("#red");
-		var bc = d3.select("#blue");
-		var oc = d3.select("#origin");
-		var dotproduct = (rc.attr("cx")-oc.attr("cx"))*(bc.attr("cx")-oc.attr("cx"))
-						+(rc.attr("cy")-oc.attr("cy"))*(bc.attr("cy")-oc.attr("cy"));
-		d3.select('#dot').text(Math.round(dotproduct));
-		var crossproduct = (rc.attr("cx")-oc.attr("cx"))*(bc.attr("cy")-oc.attr("cy"))
-						  -(rc.attr("cy")-oc.attr("cy"))*(bc.attr("cx")-oc.attr("cx"));
-		d3.select('#cross').text(Math.round(crossproduct));
+		d3.select('#dptext').text(dotproduct());
+		d3.select('#cptext').text(crossproduct());
 		d3.select('#arc').attr("d", arc);
 	});
 d3.select('svg').selectAll('circle').call(drag);
