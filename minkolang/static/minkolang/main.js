feb673a451f51ed8d026ac563efcf5b2c80ac4f2
El'endia Starman <lee314b@gmail.com>
2015-10-27 04:58:48 -0400

    Added byte and char counters.

--- minkolang/static/minkolang/main.js
+++ minkolang/static/minkolang/main.js
@@ -3,6 +3,44 @@ function encodeURL(S) {
 	return U.replace('(','%28').replace(')','%29').replace('.','%2E');
 }
 
+// Taken from https://mths.be/punycode
+function ucs2decode(string) {
+	var output = [],
+		counter = 0,
+		length = string.length,
+		value,
+		extra;
+	while (counter < length) {
+		value = string.charCodeAt(counter++);
+		if (value >= 0xD800 && value <= 0xDBFF && counter < length) {
+			// high surrogate, and there is a next character
+			extra = string.charCodeAt(counter++);
+			if ((extra & 0xFC00) == 0xDC00) { // low surrogate
+				output.push(((value & 0x3FF) << 10) + (extra & 0x3FF) + 0x10000);
+			} else {
+				// unmatched surrogate; only append this code unit, in case the next
+				// code unit is the high surrogate of a surrogate pair
+				output.push(value);
+				counter--;
+			}
+		} else {
+			output.push(value);
+		}
+	}
+	return output;
+}
+
+function updateCounts() {
+	var value = $('#code-input').val().replace(/\r\n/g, '\n');
+	var byteCount = utf8.encode(value).length; // https://mths.be/utf8js
+	var charCount = ucs2decode(value).length;
+	$('#byte-counter').text(byteCount);
+	$('#char-counter').text(charCount);
+}
+updateCounts();
+
+$('#code-input').keyup(updateCounts);
+
 function sendcode() {
 	var code = $('#code-input').val();
 	var input = $('#input-text').val();
