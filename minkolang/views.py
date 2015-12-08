b8a9e4b9edcd004a77609b91be890f7638855102
El'endia Starman <lee314b@gmail.com>
2015-12-08 00:25:57 -0500

    Further attempt...

--- minkolang/views.py
+++ minkolang/views.py
@@ -226,7 +226,7 @@ def github_view(request, *args, **kwargs):
 
     context = RequestContext(request)
 
-    with open('github_json.txt','w') as f: f.write(str(context))
+    with open('github_json.txt','w') as f: f.write(str(request))
 
     return HttpResponse("hello!", content_type="text/plain")
 
