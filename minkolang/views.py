e05c012fb6cc20e56fe8f167cda453da83ed97d1
El'endia Starman <lee314b@gmail.com>
2015-12-08 05:28:54 -0500

    I keep getting 502 errors. >_<

--- minkolang/views.py
+++ minkolang/views.py
@@ -233,8 +233,8 @@ def github_view(request, *args, **kwargs):
 ##        pass
         try:
 ##            attempt = -1
-##            attempt = subprocess.call("/home/elendia/webapps/maingit/repos/minkolang.git/pull-from-github", shell=True)
-            attempt = subprocess.call("pwd", shell=True)
+            attempt = subprocess.call("/home/elendia/bin/pull-from-github", shell=True)
+##            attempt = subprocess.call("pwd", shell=True)
             with open('github_pull_attempt.txt','w') as f: f.write("Exit code: %s"%attempt)
         except Exception as e:
 ##            pass
