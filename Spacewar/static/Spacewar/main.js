258a6b7c1546cc9bc59fa9e0701800219609c7b5
El'endia Starman <lee314b@gmail.com>
2015-12-12 20:16:33 -0500

    Added list of included js files to hidden <p>.

--- Spacewar/static/Spacewar/main.js
+++ Spacewar/static/Spacewar/main.js
@@ -1,21 +1,52 @@
+var renderLoop;
 (function($){
-	$(document).ready(function (){ console.log("main.js"); setup(); });
+	$(document).ready(function (){
+		console.log("main.js");
+		setup();
+		renderLoop = setInterval(update, 30);
+	});
 })(jQuery);
 
+var red = d3.select("#red");
+var blue = d3.select("#blue");
+
 function setup() {
-	var red = d3.select("#red");
 	red.x = 50;
 	red.y = Math.floor(500*Math.random())+50;
 	red.rot = 90;
+	red.xv = 10;
+	red.yv = 0;
 	red.attr("transform","translate("+red.x+","+red.y+"),rotate("+red.rot+")");
 	
-	var blue = d3.select("#blue");
 	blue.x = 800-50;
 	blue.y = Math.floor(500*Math.random())+50;
 	blue.rot = -90;
+	blue.xv = 0;
+	blue.yv = 0;
 	blue.attr("transform","translate("+blue.x+","+blue.y+"),rotate("+blue.rot+")");
-	
-	// var sun = d3.select("sun");
+}
+
+function update() {
+	red.x = red.x + red.xv;
+	if (red.x > 800) { red.x -= 800 } else if (red.x < 0) { red.x += 800 }
+	red.y = red.y + red.yv;
+	red.rot = red.rot + 5;
+	d3.select('#red').attr("transform","translate("+red.x+","+red.y+"),rotate("+red.rot+")");
+}
+
+function redMove(action) {
+	switch (action){
+		case "thrust":
+		break;
+		case "fire":
+		break;
+		case "turn right":
+		break;
+		case "turn left":
+		break;
+		case "hyperspace":
+		break;
+	}
 }
 
 // var svg = d3.select('svg');
