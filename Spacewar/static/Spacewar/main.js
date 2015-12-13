bd88366b5bb0bf16f52d23563239afd6874d02bf
El'endia Starman <lee314b@gmail.com>
2015-12-12 21:17:01 -0500

    Fixed anim start/stop bug.

--- Spacewar/static/Spacewar/main.js
+++ Spacewar/static/Spacewar/main.js
@@ -4,6 +4,7 @@ var renderLoop;
 		console.log("main.js");
 		setup();
 		renderLoop = setInterval(update, 30);
+		$(document).keydown(handleInput);
 	});
 })(jQuery);
 
@@ -14,7 +15,7 @@ function setup() {
 	red.x = 50;
 	red.y = Math.floor(500*Math.random())+50;
 	red.rot = 90;
-	red.xv = 10;
+	red.xv = 1;
 	red.yv = 0;
 	red.attr("transform","translate("+red.x+","+red.y+"),rotate("+red.rot+")");
 	
@@ -27,24 +28,76 @@ function setup() {
 }
 
 function update() {
-	red.x = red.x + red.xv;
-	if (red.x > 800) { red.x -= 800 } else if (red.x < 0) { red.x += 800 }
-	red.y = red.y + red.yv;
-	red.rot = red.rot + 5;
+	if (red.xv > 10){ red.xv = 10 } else if (red.xv < 0){red.xv = 0}
+	if (red.yv > 10){ red.yv = 10 } else if (red.yv < 0){red.yv = 0}
+	red.x += red.xv;
+	red.x = (red.x+800)%800;
+	red.y += red.yv;
+	red.y = (red.y+600)%600;
 	d3.select('#red').attr("transform","translate("+red.x+","+red.y+"),rotate("+red.rot+")");
+	d3.select('#blue').attr("transform","translate("+blue.x+","+blue.y+"),rotate("+blue.rot+")");
 }
 
 function redMove(action) {
 	switch (action){
 		case "thrust":
+		red.xv += Math.cos(red.rot);
+		red.yv += Math.sin(red.rot);
 		break;
 		case "fire":
+		// fireMissile("red");
 		break;
 		case "turn right":
+		red.rot = red.rot + 5;
 		break;
 		case "turn left":
+		red.rot = red.rot - 5;
 		break;
 		case "hyperspace":
+		break;
+	}
+}
+
+function handleInput(event) {
+	console.log(event.which);
+	switch (event.which){
+		// general
+		case 27:
+		clearInterval(renderLoop);
+		renderLoop = false;
+		break;
+		case 13:
+		event.preventDefault();
+		if (!renderLoop){ renderLoop = setInterval(update, 30); }
+		break
+		
+		// RED
+		case 90:
+		redMove("turn left");
+		break;
+		case 88:
+		redMove("turn right");
+		break;
+		case 67:
+		redMove("hyperspace");
+		break;
+		case 86:
+		redMove("thrust");
+		break;
+		case 66:
+		redMove("fire");
+		break;
+		
+		// BLUE
+		case 78:
+		break;
+		case 77:
+		break;
+		case 188:
+		break;
+		case 190:
+		break;
+		case 191:
 		break;
 	}
 }
