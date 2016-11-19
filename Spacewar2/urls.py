f1f839df2d1744abeb309e91196a9be731f5d91c
El'endia Starman <lee314b@gmail.com>
2016-11-19 05:53:23 -0500

    Attempt to fix repo, part 1.

--- Spacewar2/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-
-from Spacewar2.views import *
-
-urlpatterns = [
-    url(r'^$', main_view, name='main'),
-    # url(r'^github$', github_view, name='github'),
-]
