79b55ffdc5e4e13d4f69e8fed9c692ff184a31cb
El'endia Starman <lee314b@gmail.com>
2018-11-24 17:40:32 -0500

    This stinks.

--- d3applets/urls.py
+++ d3applets/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 
+from general.views import tametsi_home_view
 from d3applets.views import *
 
 urlpatterns = [
@@ -8,8 +9,6 @@ urlpatterns = [
     url(r'^varlife/shortenurl$', varlife_shortenURL, name='varlife_shortenURL'),
     url(r'^varlife/(?P<code>\w*)$', other_view, name='other_code'),
     url(r'^qftasm/permalink$', qftasm_permalink, name='qftasm_permalink'),
+    url(r'^tametsi', tametsi_home_view, name="tametsi_home"),
     url(r'^(?P<name>\w*)/', other_view, name='other'),
-    # url(r'^dotandcross/', dotandcross_view, name='dotandcross'),
-    # url(r'^radians/', radiansDemo_view, name='radiansDemo'),
-    # url(r'^gravity/', gravity_view, name='gravity'),
 ]
